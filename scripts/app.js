66b487bf8cc0311620ce3549fa3673202e89730a
Matthew <matthew.neil84@gmail.com>
2016-12-30 21:42:22 +0000

    update function names to be generic and reusable between the hero and the monster. add calc hit and check health functions. testing carried out on calc hit and check health functions, still work in progress

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,5 @@
 // create hero factory
-var createHero = function(name, health, strength) {
+var createCharacter = function(name, health, strength) {
     this.name = name || generateName();
     this.health = health || 10;
     this.strength = strength || 10;
@@ -17,16 +17,16 @@ var generateName = function() {
 };
 
 // hero attack
-var heroAttackValue = function(hero) {
-    if (hero != undefined) {
-        var atkMaxDmg = Math.max(this.hero.strength);
-        var atkDmg = Math.round(Math.random() * this.hero.strength);
+var attackValue = function(character) {
+    if (character != undefined) {
+        var atkMaxDmg = Math.max(character.strength);
+        var atkDmg = Math.round(Math.random() * character.strength);
 
-        if (atkMaxDmg != atkDmg) {
-            // basic attack damage lowest number to highest number - 1
+        if (atkDmg == atkMaxDmg) {
+            // critical attack does extra damage
             return atkDmg;
         } else {
-            // critical attack does extra damage
+            // basic attack damage lowest number to highest number - 1
             return atkDmg + 5;
         }
     } else {
@@ -34,50 +34,73 @@ var heroAttackValue = function(hero) {
     }
 };
 
+// calculate hits
+var calcHits = function(character, attackValue) {
+
+  if (character && attackValue) {
+    return character.health = character.health - attackValue;
+  }
+  else {
+    throw 'character argument is ' + character + ' and attack value is ' + attackValue;
+  }
+};
+
+// character health check
+var checkHealth = function(character) {
+  if (character.health <= 0) {
+    alert(character.name + ' is dead, try again');
+  }
+};
+
 // hero inventory
 
 // level up
-var levelUp = function(){
+var levelUp = function() {
 
 };
 
-// create monster
-var createMonster = function(){
-  // TODO: create hero and create monster are nearly the same
-  // abstract these factory functions into character, attributes functions
-  this.name = "monster";
-  this.health = 10;
-  this.strength = 10;
-};
-// monster attack
 
-// remove monster (when monster is defeated to be memory efficient)
 
 // global hero namespace
 var hero;
 var heroCreateEvent = document.getElementById('create-hero');
 
-var monster = new createMonster();
+var monster = new createCharacter('monster');
 
 heroCreateEvent.addEventListener('click', function() {
-    hero = new createHero();
-    // console.log('Your hero name is ' + hero.name,
-    //               'your attack damage is ' + hero.strength,
-    //                 'your health is ' + hero.health);
-    console.log('name: ' + hero.name);
-    console.log('health: ' + hero.health);
-    console.log('strength: ' + hero.strength);
+    if (hero == null || hero == undefined) {
+        hero = new createCharacter();
+        // console.log('Your hero name is ' + hero.name,
+        //               'your attack damage is ' + hero.strength,
+        //                 'your health is ' + hero.health);
+        console.log('name: ' + hero.name);
+        console.log('health: ' + hero.health);
+        console.log('strength: ' + hero.strength);
+    }
 });
 
+// attack events
 var attackEvent = document.getElementById('hero-attack');
 
-attackEvent.addEventListener('click', function(){
+attackEvent.addEventListener('click', function() {
 
-    // testing hero nad monster stuff works
-    var heroAttackTest = heroAttackValue(hero);
-    var monsterAttackTest = heroAttackValue(monster);
+    // hero will always attack first calculate the monster health first
+    // then carry out the monster attack
+    var heroAttack = attackValue(hero);
+    var monsterAttack = attackValue(monster);
 
-      console.log('hero attack ' + heroAttackTest);
-      console.log('monster attack ' + monsterAttackTest);
+    if (monster.health > 0) {
+      // hero attacks the monster first
+      calcHits(monster, heroAttack);
+      checkHealth(monster);
+    }
+    if (hero.health > 0 && monster.health > 0) {
+      // if the monster is not dead the monster attacks the hero
+      calcHits(hero, monsterAttack);
+      checkHealth(hero);
+    }
+
+    console.log('hero attack ' + heroAttack);
+    console.log('monster attack ' + monsterAttack);
 
 });
